2b663017d8a7497d0edbad14762b9cfc436652c2
Yoann PERIQUOI <yoann_63115@hotmail.fr>
2023-04-27 22:03:14 +0200

    test script for load charge

--- eval.py
+++ eval.py
@@ -1,20 +1,6 @@
-import requests
-import concurrent.futures
-import time
-import matplotlib.pyplot as plt
-
-def make_request():
-    url = 'http://172.16.193.38:8080/fibo/5'
-    response = requests.get(url)
-    return response.status_code
-
-
-urls = ['https://example.com/api/endpoint'] * 1000
-with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
-    futures = [executor.submit(make_request) for url in urls]
-    responses = [future.result() for future in futures]
-    concurrent.futures.wait(futures)
-
-plt.hist(responses)
-plt.show()
-plt.savefig('histogram.png')
+from locust import HttpUser, task
+class WebsiteUser(HttpUser): 
+    
+    @task 
+    def fibonacci(self): 
+        self.client.get(url='/fibo/5') 
