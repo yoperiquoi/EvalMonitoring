27f0c4ab18b238504cf655df56a435dda042206f
Yoann PERIQUOI <yoann_63115@hotmail.fr>
2023-04-27 21:32:04 +0200

    test script for load charge

--- eval.py
+++ eval.py
@@ -1,24 +1,22 @@
-import time
 import requests
-import datetime
 import concurrent.futures
+import time
+import matplotlib.pyplot as plt
 
-HOST = '172.16.193.9'
-API_PATH = '/fib/5/'
-ENDPOINT = HOST + API_PATH
-MAX_THREADS = 4
-CONCURRENT_THREADS = 3
+def make_request():
+    url = 'http://172.16.193.38:8080/fibo/5'
+    response = requests.get(url)
+    data = response.json()
+    return data
 
-def send_api_request():
-    print ('Sending API request: ', ENDPOINT)
-    r = requests.get(ENDPOINT)
-    print ('Received: ', r.status_code, r.text)
 
-start_time = datetime.datetime.now()
-print ('Starting:', start_time)
+urls = ['https://example.com/api/endpoint'] * 1000
+with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
+    futures = [executor.submit(make_request) for url in urls]
+    responses = [future.result() for future in futures]
 
-with concurrent.futures.ThreadPoolExecutor(MAX_THREADS) as executor:
-    futures = [ executor.submit(send_api_request) for x in range (CONCURRENT_THREADS) ]
-time.sleep(5)
-end_time = datetime.datetime.now()
-print ('Finished start time:', start_time, 'duration: ', end_time-start_time)
+plt.plot(responses)
+plt.xlabel('Request Number')
+plt.ylabel('Response Value')
+plt.title('API Response Plot')
+plt.show()
