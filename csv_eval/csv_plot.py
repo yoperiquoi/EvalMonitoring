e574dd45d0087c7d22daad67604dbcb89b0987b6
Yoann PERIQUOI <yoann_63115@hotmail.fr>
2023-05-04 08:27:02 +0200

    correct csv evaluation

--- csv_eval/csv_plot.py
+++ csv_eval/csv_plot.py
@@ -6,8 +6,8 @@ fig, (ax1, ax2) = plt.subplots(2, 1)
 fig.subplots_adjust(hspace=0.5)
 
 # Read the CSV file
-energy = pd.read_csv('energy_100users_fib20.csv')
-response = pd.read_csv('response_100users_fib20.csv')
+energy = pd.read_csv('energy_100users_fib5.csv')
+response = pd.read_csv('response_100users_fib5.csv')
 
 # Find the minimum and maximum time values in the response dataset
 response_min_time = response['time'].min()
@@ -28,6 +28,9 @@ ax1.axvline(x=response_max_time, color='red', linestyle='--', label='End of load
 
 # Set x axis limits for responses
 ax2.set_xlim(energy['time'].min() , energy['time'].max())
+
+# Smooth the energy line
+energy_line.set_antialiased(True)
 
 
 plt.show()
